1016dbeabf15cf7ecc9772b7a8114ddebfecb62b
sksoumik <sadmanks@gmail.com>
2018-04-16 18:20:09 +0600

    Django project for Storing Music on Cloud

---
+++ website/music/views.py
@@ -0,0 +1,14 @@
+from django.views import generic
+from .models import Album
+
+class IndexView(generic.ListView):
+    template_name = 'music/index.html'
+    context_object_name = 'all_album'
+
+    def get_queryset(self):
+        return Album.objects.all()
+
+
+class DetailView(generic.DetailView):
+    model = Album
+    template_name = 'music/detail.html'
